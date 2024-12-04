0dc875071be78055eaa3917e60493588d4caf3d5
Stef Harrington-Palmer <stef@bear.army>
2024-12-04 12:16:47 +0000

    feat: initial sweep (of the chimney)

---
+++ day_01/run.ps1
@@ -0,0 +1,28 @@
+$fileToProcess = "./full.input"
+$input = Get-Content -Path $fileToProcess
+
+$lefties = @()
+$righties = @()
+
+foreach ($line in $input) {
+    $left_item, $right_item = $line.Split()[0,-1]
+    $original_index = $input.IndexOf($line)
+    # Write-Verbose "$original_index, $left_item, $original_index, $right_item"
+    $lefties += [pscustomobject] @{'original_index' = $original_index; 'original_value' = $left_item}
+    $righties += [pscustomobject] @{'original_index' = $original_index; 'original_value' = $right_item}    
+}
+
+$lefties = $lefties | Sort-Object -Property original_value
+$righties = $righties | Sort-Object -Property original_value
+
+if ($lefties.length -ne $righties.length) {Write-Error "It's gone wrong, boss."}
+
+foreach ($line in $lefties) {
+    $left = $line.original_value
+    $right = $righties[$($lefties.IndexOf($line))].original_value
+    $difference = $right - $left
+    Write-Verbose "left is $left, right is $right, difference is $difference"
+    $total_difference += $difference
+}
+
+Write-Output "difference: $total_difference"
